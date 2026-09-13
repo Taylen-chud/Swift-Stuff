d079415db28a6528ad9a96cae5d7299705815d19
kutta <taylenwells2026@icloud.com>
2026-09-13 00:08:51 +0000

    forgot about this

---
+++ Remember.swift
@@ -0,0 +1,25 @@
+// Swift is a type-safe language, so we can use a struct to represent a "Remember" object. This struct will have properties for the title, description, and date of the memory.
+// use double for math
+// use CGFloat for UI
+
+// numbers can be Int, Double, Float, etc.
+// eg. 
+// let myInt = 42
+// let myDouble = 3.14
+// let myFloat = 2.718
+
+// boolean values can be true or false
+// you can use the Date type to represent a specific point in time
+// Variables can change value
+// let = to one thing and cannot be changed
+// import Foundation for practically all swift programs so far
+
+// if you do something like var myNumber for example and it is a double, you can change it to another double value but not to a string or an int. You can also use the Date type to represent a specific point in time.
+// and if whatever value = to a whole number it will still be an double because var = a double and not an int. You can also use the Date type to represent a specific point in time.
+// if it is declared as a double, it will always be a double and cannot be changed to an int or a string. You can also use the Date type to represent a specific point in time.
+// == is equal to, != is not equal to, > is greater than, < is less than, >= is greater than or equal to, <= is less than or equal to. You can also use the Date type to represent a specific point in time.
+// if its changing make it a var
+// if its not changing make it a let
+
+// do stuff like likeCount += 1 rather than LikeCount + 1 because it is more efficient and easier to read. You can also use the Date type to represent a specific point in time.
+// Swift uses PEMDAS for order of operations, so you can use parentheses to group operations and control the order in which they are performed. You can also use the Date type to represent a specific point in time.
