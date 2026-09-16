0266938de3d8fea7bd7bb3bedd5146507fd4ef96
taylen-chud <taylenwells2026@icloud.com>
2026-09-16 17:24:55 -0600

    protocals and for loops

---
+++ ForLoops.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+for x in 0..<100 {
+    print(x)
+}
+
+let myArray = ["Alpha", "Beta", "Gamma", "Delta", "Epsilon"]
+
+var secondArray: [String] = []
+
+for item in myArray {
+    print(item)
+
+    if item == "Gamma" {
+        secondArray.append(item)
+    }
+}
+
+print(secondArray)
+
+struct LessonModel {
+    let title: String
+    let isFavorite: Bool
+}
+
+let allLessons = [
+    LessonModel(title: "Lesson 1", isFavorite: true),
+    LessonModel(title: "Lesson 2", isFavorite: false),
+    LessonModel(title: "Lesson 3", isFavorite: false),
+    LessonModel(title: "Lesson 4", isFavorite: false),
+    LessonModel(title: "Lesson 5", isFavorite: true),
+]
+
+var favoriteLessons: [LessonModel] = []
+for lesson in allLessons {
+    if lesson.isFavorite {
+        favoriteLessons.append(lesson)
+
+    }
+}
+
+print(favoriteLessons)
+
+for (index, lesson) in allLessons.enumerated() {
+
+    if index == 1{
+        break
+    }
+    print("index: \(index) || lesson: \(lesson)")
+}
